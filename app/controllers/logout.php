3967cdc3a6ce48277bd95bc56031c400d28a1ece
Juan Sebastian <juans.quintoh@hotmail.com>
2025-10-09 10:24:52 -0500

    Migracion a frontend moderno: Eliminado codigo PHP legacy, agregado React + TypeScript

--- app/controllers/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/GestiondeTareas/app/config/dirs.php');
-require_once(CONTROLLERS_PATH . '/AuthController.php');
-
-$auth = new AuthController();
-$auth->logout();
